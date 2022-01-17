833a0f2c2b8ced416946735fb9da5a5043be97d3
Luzanov Yevgen <luzanov.zena@gmail.com>
2022-01-17 21:02:53 +0200

    extension user database

--- project/forms.py
+++ project/forms.py
@@ -1,7 +1,17 @@
 from starlette_wtf import StarletteForm
-from wtforms import StringField
-from wtforms.validators	import DataRequired
+from wtforms import StringField, BooleanField, EmailField, PasswordField
+from wtforms.validators	import DataRequired, Email
 
 class User(StarletteForm):
     name = StringField('name', validators=[DataRequired()])
+    admin= BooleanField('admin')
+    email=EmailField('email', validators=[Email()])
+    password=PasswordField()
+
+
+    def is_admin(self):
+        if self.admin==True:
+            return True
+        else:
+            return False
     
